3bf9cb68d3e238285fac8e571e4a25d97b734cce
Stephen Gravitt <Stephen@Grav-IT.net>
2019-11-09 13:52:00 -0700

    output data test

--- script.js
+++ script.js
@@ -192,7 +192,6 @@ function getFieldValue (inputID, inputType){
 
 }
 
-
 //function that handles adding onchange and/or onclick events, different for different field types
 function detectFieldChange ( inputID, inputType ){
     if ( inputType == "text" ){
@@ -239,46 +238,125 @@ function updateData(inputID, inputValue){
     
 }
 
-//start watching all form fields we're tracking, load initial values seen on page (as this code loads at page footer)
-for(var i = 0; i < inputData.length; i++) {
-    detectFieldChange ( inputData[i].field_id, inputData[i].field_type );
-    updateData(inputData[i].field_id, getFieldValue( inputData[i].field_id , inputData[i].field_type ));
+//change final form redirect behavior
+function changeFormRedirect(){
+
+    $("#comp-k2qivcerform").submit(function(e) {
+        //grab collective form data
+        var updatedData = JSON.parse(getCookie('inputData'));
+        
+        //verify form data was received correctly
+        if( updatedData ){
+            e.preventDefault();
+            var form = $(this);
+            
+            var redirectPage
+            //verify the most valuable attribute
+            updatedData.forEach(function(inputField){
+                if(inputField.field_name == 'attribute'){
+                    redirectPage = inputField.value;
+                }
+                
+            });
+            var redirectURL
+            //grab the correct page_url for redirection based on selection
+            successPages.forEach(function(pages){
+                if(pages.page_name === redirectPage){
+                    redirectURL = pages.page_url;
+                }    
+            });
+            console.log(redirectURL);
+            //default to speed page if somehow we've failed to get the proper page.
+            if(!redirectURL){redirectURL = "/success-speed" };
+    
+            $.ajax({
+                data: form.serialize(), // serializes the form's elements.
+                success: function(data){
+                    window.location.href = redirectURL;
+                }
+            });
+        }
+    });
 }
 
-
-$("#comp-k2qivcerform").submit(function(e) {
-    //grab collective form data
-    var updatedData = JSON.parse(getCookie('inputData'));
-    
-    //verify form data was received correctly
-    if( updatedData ){
-        e.preventDefault();
-        var form = $(this);
-        
-        var redirectPage
-        //verify the most valuable attribute
-        updatedData.forEach(function(inputField){
-            if(inputField.field_name == 'attribute'){
-                redirectPage = inputField.value;
-            }
-            
-        });
-        var redirectURL
-        //grab the correct page_url for redirection based on selection
-        successPages.forEach(function(pages){
-            if(pages.page_name === redirectPage){
-                redirectURL = pages.page_url;
-            }    
-        });
-        console.log(redirectURL);
-        //default to speed page if somehow we've failed to get the proper page.
-        if(!redirectURL){redirectURL = "/success-speed" };
-
-        $.ajax({
-            data: form.serialize(), // serializes the form's elements.
-            success: function(data){
-                window.location.href = redirectURL;
-            }
-        });
+//start watching all form fields we're tracking, load initial values seen on page (as this code loads at page footer)
+function initDataTracking(){
+    for(var i = 0; i < inputData.length; i++) {
+        detectFieldChange ( inputData[i].field_id, inputData[i].field_type );
+        updateData(inputData[i].field_id, getFieldValue( inputData[i].field_id , inputData[i].field_type ));
     }
-});
+}
+
+//is this a pre-defined results page?
+function getOutputBox(pagePath){
+    var parentBox = false;
+    for(var i = 0; i < successPages.length; i++) {
+        
+        if(successPages[i].page_url === pagePath){
+            parentBox = successPages[i].parent_box;
+            break;
+        }
+    }
+    return parentBox;
+}
+
+function getValueByName(jsonArray, field_name){
+    for(var i = 0; i < jsonArray.length; i++) {
+        if(jsonArray[i].field_name){
+            return jsonArray[i].value;
+        }
+    }
+}
+
+function outputData(parentBox){
+    var outputEl = document.getElementById(parentBox);
+    var cookieData = JSON.parse(getCookie('inputData'));
+    var outputHTML = "";
+    outputHTML += "<div id=\"sportos-results\">";
+        outputHTML += "<div class=\"results-col col1\">";
+            outputHTML += "<div class=\"gender-relationship sportos-block\">";
+                outputHTML += "<h3 class=\"sportos-gender\" >" + getValueByName(cookieData, "gender") + "</h3>";
+                outputHTML += "<h3 class=\"sportos-relationship\" >" + getValueByName(cookieData, "relationship") + "</h3>";
+            outputHTML +="</div><!--/.gender-relationship-->"
+            outputHTML += "<div class=\"age-location sportos-block\">";
+                outputHTML += "<h3 class=\"sportos-age\" >" + getValueByName(cookieData, "age") + "</h3>";
+                outputHTML += "<h3 class=\"sportos-location\" >" + getValueByName(cookieData, "location") + "</h3>";
+            outputHTML +="</div><!--/.age-location-->"
+            outputHTML += "<div class=\"focus sportos-block\" >";
+                var focusVals = new Array;
+                focusVals = getValueByName(cookieData, "focus");
+                if(focusVals.length < 0 ){
+                    for ( i=0 ; focusVals.length > i ; i++){
+                        outputHTML += "<div class=\"sportos-focus-item\" >" + focusVals[i] + "</div>";
+                    }
+                }
+            outputHTML += "</div><!--/.focus-->";
+        outputHTML += "</div><!--/.results-col.col1-->";
+        outputHTML += "<div class=\"results-col col2\">";
+            outputHTML += "<div class=\"sportos-block goals\">";
+                outputHTML += "<h3 class=\"goals sportos-label\">Goals</h3>";
+                outputHTML += "<div class=\"tool-tip-container\"><img src=\"https://static.wixstatic.com/media/b42ed2_7571210680424427a4dbc7a87b152500~mv2.png/v1/fill/w_24,h_26,al_c,q_80/icon-question-rev.jpg\"><div class=\"tool-tip-inner\"><p>Here is some tooltip</p></div></div>";
+                outputHTML += "<p class=\"goals sportos-value\">" + getValueByName(cookieData, "goals") + "</p>";
+            outputHTML += "</div><!--/.sportos-block.goals-->";
+            outputHTML += "<div class=\"sportos-block motivation\">";
+                outputHTML += "<h3 class=\"motivation sportos-label\">Motivation</h3>";
+                outputHTML += "<div class=\"tool-tip-container\"><img src=\"https://static.wixstatic.com/media/b42ed2_7571210680424427a4dbc7a87b152500~mv2.png/v1/fill/w_24,h_26,al_c,q_80/icon-question-rev.jpg\"><div class=\"tool-tip-inner\"><p>Here is some tooltip</p></div></div>";
+                outputHTML += "<p class=\"motivation sportos-value\">" + getValueByName(cookieData, "motivation") + "</p>";
+            outputHTML += "</div><!--/.sportos-block.motivation-->";
+            outputHTML += "<div class=\"sportos-block.values\">";
+                var valueVals = new Array;
+                valueVals = getValueByName(cookieData, "values");
+                outputHTML += "<h3 class=\"values sportos-label\">Values</h3>";
+                outputHTML += "<div class=\"tool-tip-container\"><img src=\"https://static.wixstatic.com/media/b42ed2_7571210680424427a4dbc7a87b152500~mv2.png/v1/fill/w_24,h_26,al_c,q_80/icon-question-rev.jpg\"><div class=\"tool-tip-inner\"><p>Here is some tooltip</p></div></div>";
+                outputHTML += "<div class=\"values sportos-value\">";
+                for ( i=0 ; valueVals.length > i ; i++){
+                    outputHTML += "<div class=\"sportos-values-item\" >" + valueVals[i] + "</div>";
+                }
+                outputHTML += "</div><!--/.values.sportos-value-->";
+            outputHTML += "</div><!--/.sportos-block.values-->";
+        outputHTML += "</div><!--/.results-col.col2-->";
+    outputHTML += "</div><!--#sportos-results-->";
+    outputEl.innerHTML = outputHTML;
+
+}
+
