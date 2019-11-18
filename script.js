7ef10c097645932a1a1b09ffdab063c455405deb
Stephen Gravitt <Stephen@Grav-IT.net>
2019-11-18 11:18:36 -0700

    fixed bug where url param data needs to be json parsed twice, once extra for the escaping/encoding aspect

--- script.js
+++ script.js
@@ -360,6 +360,10 @@ function outputData(parentBox){
     outputEl.setAttribute("id",parentBox); */
     outputEl.classList.add("rendered");
     var cookieData = JSON.parse(getCookie('inputData'));
+    
+    //verify if string and try to re-parse, double stringification happening with escaped url params
+    if(cookieData.constructor === "isString?".constructor){cookieData = JSON.parse(input2);}
+
     var outputHTML = "";
     outputHTML += "<div id=\"sportos-results\">";
         outputHTML += "<div class=\"results-col col1\">";
